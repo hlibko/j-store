23bf9fb353f7d7dbbbebdd33b88cb2b6279d559f
Hlib Kochubei <hlib.kochubei@gmail.com>
2025-05-04 12:20:18 +0200

    Add unit tests for the infrastructure stack

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,17 +1,121 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infrastructure from '../lib/infrastructure-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import * as Infrastructure from '../lib/infrastructure-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infrastructure-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('InfrastructureStack', () => {
+  let app: cdk.App;
+  let stack: Infrastructure.InfrastructureStack;
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new Infrastructure.InfrastructureStack(app, 'TestStack');
+    template = Template.fromStack(stack);
+  });
+
+  test('S3 Bucket Created with Correct Configuration', () => {
+    // Verify the S3 bucket is created with the correct properties
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      WebsiteConfiguration: {
+        IndexDocument: 'index.html',
+        ErrorDocument: 'index.html'
+      },
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true
+      }
+    });
+  });
+
+  test('CloudFront Origin Access Identity Created', () => {
+    // Verify the CloudFront OAI is created
+    template.resourceCountIs('AWS::CloudFront::CloudFrontOriginAccessIdentity', 1);
+    
+    template.hasResourceProperties('AWS::CloudFront::CloudFrontOriginAccessIdentity', {
+      CloudFrontOriginAccessIdentityConfig: {
+        Comment: 'Allow CloudFront to access the website bucket'
+      }
+    });
+  });
+
+  test('S3 Bucket Policy Includes GetObject Permission', () => {
+    // Check that there's a bucket policy with s3:GetObject permission
+    template.hasResourceProperties('AWS::S3::BucketPolicy', {
+      PolicyDocument: {
+        Statement: Match.arrayWith([
+          Match.objectLike({
+            Action: 's3:GetObject',
+            Effect: 'Allow',
+            Principal: {
+              CanonicalUser: Match.anyValue()
+            }
+          })
+        ])
+      }
+    });
+  });
+
+  test('CloudFront Distribution Created with Correct Configuration', () => {
+    // Verify the CloudFront distribution is created with the correct properties
+    template.hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        DefaultRootObject: 'index.html',
+        Origins: [
+          {
+            S3OriginConfig: {
+              OriginAccessIdentity: {
+                'Fn::Join': [
+                  '',
+                  [
+                    'origin-access-identity/cloudfront/',
+                    {
+                      'Ref': Match.stringLikeRegexp('OriginAccessIdentity')
+                    }
+                  ]
+                ]
+              }
+            }
+          }
+        ],
+        DefaultCacheBehavior: {
+          AllowedMethods: ['GET', 'HEAD', 'OPTIONS'],
+          Compress: true,
+          ViewerProtocolPolicy: 'redirect-to-https'
+        },
+        CustomErrorResponses: [
+          {
+            ErrorCode: 404,
+            ResponseCode: 200,
+            ResponsePagePath: '/index.html'
+          }
+        ]
+      }
+    });
+  });
+
+  test('S3 Bucket Deployment Resource Created', () => {
+    // Verify the S3 deployment resource is created
+    template.resourceCountIs('Custom::CDKBucketDeployment', 1);
+  });
+
+  test('CloudFront Domain Name Output Created', () => {
+    // Verify the CloudFront domain name output is created
+    template.hasOutput('DistributionDomainName', {
+      Description: 'The domain name of the CloudFront distribution'
+    });
+  });
+
+  test('S3 Bucket Name Output Created', () => {
+    // Verify the S3 bucket name output is created
+    template.hasOutput('WebsiteBucketName', {
+      Description: 'The name of the S3 bucket'
+    });
+  });
+  
+  test('Auto Delete Objects Custom Resource Created', () => {
+    // Verify the custom resource for auto-deleting objects is created
+    template.resourceCountIs('Custom::S3AutoDeleteObjects', 1);
+  });
 });
