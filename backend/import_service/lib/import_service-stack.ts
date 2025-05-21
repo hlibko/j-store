bdb7473b0f36686fcfff943c1976d4ee712f0ecc
Hlib Kochubei <hlib.kochubei@gmail.com>
2025-05-21 20:13:50 +0200

    Create and configure a new S3 bucket with a folder called 'uploaded'

--- backend/import_service/lib/import_service-stack.ts
+++ backend/import_service/lib/import_service-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class ImportServiceStack extends cdk.Stack {
+  public readonly importBucket: s3.Bucket;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create S3 bucket for import service
+    this.importBucket = new s3.Bucket(this, 'ImportBucket', {
+      bucketName: `j-store-import-service-bucket-${this.account}-${this.region}`,
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+          ],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+        },
+      ],
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // For development only, use RETAIN for production
+      autoDeleteObjects: true, // For development only, remove for production
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create 'uploaded' folder in the bucket
+    new s3deploy.BucketDeployment(this, 'DeployUploadedFolder', {
+      sources: [s3deploy.Source.data('uploaded/.keep', '')], // Create an empty .keep file in the uploaded folder
+      destinationBucket: this.importBucket,
+    });
+
+    // Output the bucket name
+    new cdk.CfnOutput(this, 'ImportBucketName', {
+      value: this.importBucket.bucketName,
+      description: 'The name of the S3 bucket for import service',
+      exportName: 'ImportBucketName',
+    });
   }
 }
