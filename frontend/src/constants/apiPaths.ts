a03789272f56a2348c4a9f2bb5cba05eb21bb33c
Hlib Kochubei <hlib.kochubei@gmail.com>
2025-05-27 08:11:15 +0200

    Integrate 'importProductsFile' endpoint with frontend

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://j7ogu0eay3.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://isewdetoni.execute-api.eu-north-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
