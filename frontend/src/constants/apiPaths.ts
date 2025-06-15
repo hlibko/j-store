9200edb48564150280f7bba53cd3ba69cebcedfd
Hlib Kochubei <hlib.kochubei@gmail.com>
2025-06-15 11:22:19 +0200

    Update paths for frontend

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://jmq80uj6zf.execute-api.eu-north-1.amazonaws.com/prod/api/profile/cart",
   import: "https://j7ogu0eay3.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://isewdetoni.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://jmq80uj6zf.execute-api.eu-north-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
